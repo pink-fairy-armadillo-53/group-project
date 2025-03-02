7fe28b68a49ce9d116e850d72e49b51e908d976a
Gerald Ingraham <gingraham@icloud.com>
2025-03-01 19:29:28 -0500

    removed console.logs used for testing

--- client/src/services/fetchMovie.js
+++ client/src/services/fetchMovie.js
@@ -1,12 +1,10 @@
 export const fetchTopRatedMovies = async ()=>{
-try{
-    const response = await fetch("http://localhost:3000/api/movies/top-rated")
-    
-   const data = await response.json()
-   console.log(data)
-  
-}catch(error){
-    console.log("error from the client: services/fetchMovie")
-}
+    try{
+        const response = await fetch("http://localhost:3000/api/movies/top-rated")
+    const data = await response.json()
+    return data
+    }catch(error){
+        console.log("error from the client: services/fetchMovie")
+    }
 }
 fetchTopRatedMovies()
