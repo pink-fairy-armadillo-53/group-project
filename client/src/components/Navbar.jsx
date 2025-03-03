d7d01a79ae7635772bd5bffbb2a73b4b31a54fa9
Gerald Ingraham <gingraham@icloud.com>
2025-03-02 23:34:28 -0500

    updated navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,17 +1,13 @@
 // Child component that displays nav bar (for generic home page and logged in users) and search bar
 
-import React , { useRef }from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import { FaRegUser, FaRegHeart } from "react-icons/fa";
+
 //This is just a temporary navbar for testing routes. Feel free to edit or replace with your navbar later. -Arthur
-
-
 const Navbar = () => {
   return (
-    <nav className='navbar' >
-        <div className="logo"><a href="#">Rotten</a></div>
-
-      <ul className='navbar-left'>
+    <nav className='navbar'>
+      <ul>
         <li>
           <Link to='/'>Home</Link>
         </li>
@@ -19,23 +15,12 @@ const Navbar = () => {
           <Link to='/login'>Login/Register</Link>
         </li>
         <li>
-        <Link to='/movies'>Movies</Link>
+          <Link to='/favorites'>Favorites</Link>
         </li>
         <li>
-          <Link to='/my-reviews'>Reviews</Link>
+          <Link to='/my-reviews'>My Reviews</Link>
         </li>
       </ul>
-
-      <ul className='navbar-right'>
-        <li><input type="search" id="site-search" name="q" /></li>
-        <li>
-        <Link to='/favorites'><FaRegHeart/></Link>
-        </li>
-        <li>
-          <Link to='/login'><FaRegUser/></Link>
-        </li>
-      </ul>
-
     </nav>
   );
 };
