82def063b698557a740bf1e3efa29cdec830d043
reva2024 <revu4143@gmail.com>
2025-03-03 13:21:08 -0800

    navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,11 +2,9 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { FaRegUser, FaRegHeart } from "react-icons/fa"; //User and favorite icons
 import { FiMenu, FiX } from "react-icons/fi"; // Hamburger and close icons
-
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
   const [scrolled, setScrolled] = useState(false);
-
   // Function to track scroll position
   useEffect(() => {
     const handleScroll = () => {
@@ -16,22 +14,18 @@ const Navbar = () => {
         setScrolled(false);
       }
     };
-
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
-
   return (
     <nav className={`navbar ${scrolled ? "scrolled" : ""}`}>
       <div className="logo">
         <a href="#">Rotten</a>
       </div>
-
       {/* Hamburger Menu Icon */}
       <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
         {menuOpen ? <FiX /> : <FiMenu />}
       </div>
-
       {/* Navbar Links */}
       <div className={`navbar-links-container ${menuOpen ? "active" : ""}`}>
         <ul className="navbar-left">
@@ -40,7 +34,6 @@ const Navbar = () => {
           <li><Link to="/movies" onClick={() => setMenuOpen(false)}>Movies</Link></li>
           <li><Link to="/my-reviews" onClick={() => setMenuOpen(false)}>Reviews</Link></li>
         </ul>
-
         <ul className="navbar-right">
           <li><input type="search" id="site-search" name="q" placeholder="Search..." /></li>
           <li><Link to="/favorites" onClick={() => setMenuOpen(false)}><FaRegHeart /></Link></li>
@@ -50,5 +43,4 @@ const Navbar = () => {
     </nav>
   );
 };
-
 export default Navbar;
