6b4f9561f8306c3ad1dce816fa34bf2e9d8607f8
Gerald Ingraham <gingraham@icloud.com>
2025-03-02 19:57:22 -0500

    server updated

--- server/server.js
+++ server/server.js
@@ -2,13 +2,19 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
-
+const cors = require('cors')
 //movie controller
 const movieController = require('./controllers/movieController')
 
 // Middleware to parse JSON bodies
 app.use(express.json());
 
+app.use(cors({
+    origin: 'http://localhost:5173', // Allow only your frontend
+    methods: 'GET,POST,PUT,DELETE',
+    credentials: true, // Allow cookies (if needed)
+  }));
+  
 // movie controller for top rated movies
 app.get('/api/movies/top-rated', movieController.getTopRatedMovies, (req, res) => {
     res.status(200).send(res.locals.topMovies.results)
